e01083a16489da0cbbc7070a58813d18267ab22d
Pieter Wigboldus <pieter@way2web.nl>
2017-10-07 14:10:56 +0200

    Convert multiple files

--- server/index.js
+++ server/index.js
@@ -1,38 +1,51 @@
 const express = require('express');
-const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
+const multer = require('multer');
 const app = express();
 var Converter = require('ppt-png');
-var glob = require('glob');
 
 require('array-helpers');
 
-app.use(fileUpload());
+app.use(bodyParser.json());
+
+
+var storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, './upload');
+    },
+    filename: function(req, file, callback) {
+        callback(null, file.fieldname + '-' + Date.now());
+    }
+});
+
+var upload = multer({
+    storage: storage
+}).array('ppt', 2);
+
 
 app.post('/upload', function(req, res) {
-    if (!req.files || !req.files.ppt) {
-        return res.status(400).send('No files were uploaded.');
-    }
-
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-    let ppt = req.files.ppt;
-    let invert = false;
-    let greyscale = false;
-
-    if(req.body) {
-        invert = req.body.invert || false;
-        greyscale = req.body.greyscale || false;
-    }
-
-  // Use the mv() method to place the file somewhere on your server
-    ppt.mv('upload/tmp.ppt', function(err) {
-        if (err) {
-            return res.status(500).send(err);
+    upload(req, res, function(err) {
+        if(err) {
+            return res.end('Error uploading file.');
         }
 
-        res.send('File uploaded!');
-    });
+        let ppt = req.files;
+        let invert = false;
+        let greyscale = false;
 
-    process(invert, greyscale);
+        if (!ppt || ppt.length < 1) {
+            return res.status(400).send('No files were uploaded.');
+        }
+
+        if(req.body) {
+            invert = req.body.invert || false;
+            greyscale = req.body.greyscale || false;
+        }
+
+        process(req.files, invert, greyscale);
+
+        res.end('File is uploaded');
+    });
 });
 
 app.use(express.static('public'));
@@ -44,37 +57,29 @@ app.listen(3000, function() {
 /**
  * Process the ppt.
  *
+ * @param {array} files
  * @param {boolean} invert
  * @param {boolean} greyscale
  */
-function process(invert, greyscale) {
+function process(files, invert, greyscale) {
     console.log('invert:' + invert + '|greyscale:' + greyscale);
-    glob('upload/tmp.ppt', {}, convert);
 
-    /**
-     * Convert the ppt to png.
-     *
-     * @param {object} error
-     * @param {array} files
-     */
-    function convert(error, files) {
-        console.log('files: ', files.length);
-        if(files) {
-            new Converter({
-                files:          files,
-                output:         'converted/',
-                invert:         invert || false,
-                greyscale:      greyscale || false,
-                deletePdfFile:  true,
-                outputType:     'png',
-                logLevel:       2,
-                fileNameFormat: '_vers_%d'
-            }).wait().then(function(data) {
-                console.log(data.failed, data.success.length, data.files.length, data.time);
-                if(data.failed.length > 0) {
-                    convert(null, data.failed.multikey('file'));
-                }
-            });
-        }
+    console.log('files: ', files.length);
+    if(files) {
+        new Converter({
+            files:          files,
+            output:         'converted/',
+            invert:         invert || false,
+            greyscale:      greyscale || false,
+            deletePdfFile:  true,
+            outputType:     'png',
+            logLevel:       2,
+            fileNameFormat: '_vers_%d'
+        }).wait().then(function(data) {
+            console.log(data.failed, data.success.length, data.files.length, data.time);
+            if(data.failed.length > 0) {
+                convert(null, data.failed.multikey('file'));
+            }
+        });
     }
 }
